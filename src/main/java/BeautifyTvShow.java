b799df7a01138aeaad7457352a20f19c9b2f414e
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2019-11-08 20:44:02 -0500

    some of incorrect options and argumetns handling done

--- src/main/java/BeautifyTvShow.java
+++ src/main/java/BeautifyTvShow.java
@@ -10,34 +10,35 @@ public class BeautifyTvShow {
 	public static void main(String[] args) throws FileNotFoundException {
 		Thread[] DirThread = new Thread[args.length];
 		CLIOptions opt = new CLIOptions(args);
-		for (int i = 0; i < args.length; i++) {
-			try {
-				DirThread[i] = new Thread(new MultiThreading(args[i]));
-				DirThread[i].start();
-			} catch(FileNotFoundException e) {
-				System.out.printf("\n\n!!File or Directory '%s' does not exist!!\n\n", args[i]);
-			}
-		}
-		for (Thread individualThread : DirThread) {
-			try {
-				if (individualThread != null) {
-					individualThread.join();
-				}
-			} catch (InterruptedException e) {
-				System.out.printf("join failed!\n");
-				e.printStackTrace();
-			}
-		}
-		System.out.printf("All threads joined succesfully!\n");
-		Properties props = new Properties();
-		Properties sensitive = new Properties();
-		try {
-			props.load(BeautifyTvShow.class.getClassLoader().getResourceAsStream("application.properties"));
-			sensitive.load(BeautifyTvShow.class.getClassLoader().getResourceAsStream("sensitive.properties"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//System.out.printf("api url: %s\napi key: %s\n", props.getProperty("api.searchtv.url"), sensitive.getProperty("api.key"));
+		return;
+//		for (int i = 0; i < args.length; i++) {
+//			try {
+//				DirThread[i] = new Thread(new MultiThreading(args[i]));
+//				DirThread[i].start();
+//			} catch(FileNotFoundException e) {
+//				System.out.printf("\n\n!!File or Directory '%s' does not exist!!\n\n", args[i]);
+//			}
+//		}
+//		for (Thread individualThread : DirThread) {
+//			try {
+//				if (individualThread != null) {
+//					individualThread.join();
+//				}
+//			} catch (InterruptedException e) {
+//				System.out.printf("join failed!\n");
+//				e.printStackTrace();
+//			}
+//		}
+//		System.out.printf("All threads joined succesfully!\n");
+//		Properties props = new Properties();
+//		Properties sensitive = new Properties();
+//		try {
+//			props.load(BeautifyTvShow.class.getClassLoader().getResourceAsStream("application.properties"));
+//			sensitive.load(BeautifyTvShow.class.getClassLoader().getResourceAsStream("sensitive.properties"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		//System.out.printf("api url: %s\napi key: %s\n", props.getProperty("api.searchtv.url"), sensitive.getProperty("api.key"));
 	}
 }
--- src/main/java/CLIOptions.java
+++ src/main/java/CLIOptions.java
@@ -16,30 +16,54 @@ public class CLIOptions {
 		allOpt.add(new Opts('h', 0));
 		allOpt.add(new Opts('m', 0));
 		allOpt.add(new Opts('d', 1));
-		if (args[0].equals("-h") || args[0].contentEquals("--help")) {
-			printHelpPage();
-			return;
-		}
+//		if (args[0].equals("-h") || args[0].contentEquals("--help")) {
+//			printHelpPage();
+//			return;
+//		}
 		for (int i = 0; i < args.length; i++) {
-			if (args[i].charAt(0) == '-') {
+			if (args[i].charAt(0) == '-') { //handle option declaration
 				Opts currOpt = null;
-				if (args[i].charAt(1) == '-') {
+				if (args[i].charAt(1) == '-') { //handle full name declaration
 					currOpt = matchOpt(args[i].charAt(2));
+					currOpt.setValue(true);
 					try {
 						currOpt.addOpts(args[i].split("=")[1]);
 					} catch (IndexOutOfBoundsException e) {
 						System.out.printf("Wrong format passed for %s. Expected format is:\n--option=argument\n", args[i]);
 					}
-				} else {
+				} else { //handle short notation
 					currOpt = matchOpt(args[i].charAt(1));
-					for (i++; i < i + currOpt.getNumOpt(); i++) {
-						currOpt.addOpts(args[i]);
+					currOpt.setValue(true);
+					int temp = ++i;
+					try {
+						for (; i < temp + currOpt.getNumOpt(); i++) {
+							if (args[i].charAt(0) == '-') {
+								currOpt.setValue(false);
+								throw new IndexOutOfBoundsException();
+							} else {
+								currOpt.addOpts(args[i]);
+							}
+						}
+					} catch (IndexOutOfBoundsException e) {
+						System.out.printf("wrong use of options %s. Expected %d argument(s) after the option declaration but got %d\n", args[i-1], currOpt.getNumOpt(), currOpt.getLastIndex());
 					}
 					i--; // to not skip record which comes after all of the arguments of an option are parsed
 				}
 			} else {
 				dirs2beautify.add(args[i]);
 			}
+		}
+		for (int i = 0; i < this.allOpt.size(); i++) {
+			Opts temp = this.allOpt.get(i);
+			System.out.printf("option %d : %c | active/not active: %b | number of options: %d\n", i, temp.getName(), temp.isValue(), temp.getLastIndex());
+			if (temp.getLastIndex() != 0) {
+				for (int j = 0; j < temp.getLastIndex(); j++) {
+					System.out.printf("option %d is %s\n", j, temp.getOpts()[j]);
+				}
+			}
+		}
+		for (int i = 0; i < this.dirs2beautify.size(); i++) {
+			System.out.printf("dirs to beautify: %s\n", this.dirs2beautify.get(i));
 		}
 	}
 
