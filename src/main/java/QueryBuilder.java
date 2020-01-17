1967508bcf668e9ae49dad99b8531d7a69d1d300
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2020-01-17 12:05:07 -0500

    finished CLI options handling

--- src/main/java/QueryBuilder.java
+++ src/main/java/QueryBuilder.java
@@ -5,9 +5,8 @@ import java.net.URLEncoder;
 import java.util.Map;
 
 public class QueryBuilder {
-
+	//build the query string of the form: opt1=val1&opt2=val2&...
 	public static String formatParams(Map<String, String> params) {
-		
 		StringBuilder formatted = new StringBuilder();
 		//iterating through the map to get each param
 		for(Map.Entry<String, String> param : params.entrySet()) {
@@ -23,9 +22,9 @@ public class QueryBuilder {
 		}
 		return (formatted.toString().length() > 0) 
 				? formatted.deleteCharAt(formatted.length()-1).toString() 
-				: formatted.toString();
+				: formatted.toString(); //delete the trailing &
 	}
-	
+	//return the full url as a sting exampl.com/index.html?opt1=val1&opt2=val2&...
 	public static String buildURL(String baseurl, String query_params) {
 		return baseurl + "?" + query_params;
 	}
